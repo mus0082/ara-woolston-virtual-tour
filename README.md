f651135122eae4a79294b930df0ae04036db1aa3
Julian <uzairuddin95@hotmail.com>
2022-10-04 16:50:34 +1300

    add source-code files and assest of Virtual tour

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ara-woolston-virtual-tour
+
+# Responsible for developed the full Virtual-tour for Ara Institute of Canterbury, which involves custom coding like JavaScript, HTML 5, CSS and developed the whole virtual tour on Lapentor Software. There is a QR-code given which helps to preview the whole Virtual tour. Separate files for coding.
---
+++ uploads/vr/609076545b1be81e432fba66/pano/vtour/60973acb4b455c42de7a09b2/scene.php
@@ -0,0 +1,43 @@
+<?php
+     $url = explode('/uploads/vr', $_SERVER['REQUEST_URI']);
+     //var_dump($url[0]);
+     header('Content-Type: text/xml');
+?>
+<krpano version="1.19" title="Virtual Tour">
+    <!-- set skin settings: bingmaps? gyro? thumbnails? tooltips? -->
+    <skin_settings maps="false" maps_type="bing" maps_bing_api_key="" maps_zoombuttons="false" gyro="true" title="true" thumbs="true" thumbs_width="120" thumbs_height="80" thumbs_padding="10" thumbs_crop="0|40|240|160" thumbs_opened="false" thumbs_text="false" thumbs_dragging="true" thumbs_onhoverscrolling="false" thumbs_scrollbuttons="false" thumbs_scrollindicator="false" thumbs_loop="false" tooltips_thumbs="false" tooltips_hotspots="false" tooltips_mapspots="false" loadscene_flags="MERGE" loadscene_blend="BLEND(0.5)" controlbar_offset="20"/>
+    <control mousetype="drag2d" mouseaccelerate="0.2"/>
+
+    <include url="<?php echo $url[0]?$url[0]:'' ?>/bower_components/krpano/extend.xml" />
+    <view  fovtype="MFOV" fov="120" maxpixelzoom="0" fovmin="70" fovmax="150" />
+    <preview url="preview.jpg" />
+
+    <image if="!webvr.isenabled">
+        <cube url="pc/pc_%s.jpg" />
+        <cube url="mobile/mobile_%s.jpg" devices="mobile" />
+    </image>
+
+    <image if="webvr.isenabled">
+        <cube url="vr/vr_%s.jpg" />
+    </image>
+
+        <action name="draghotspot">
+            <![CDATA[
+            if(%1 != dragging,
+            spheretoscreen(ath, atv, hotspotcenterx, hotspotcentery);
+            sub(drag_adjustx, mouse.stagex, hotspotcenterx);
+            sub(drag_adjusty, mouse.stagey, hotspotcentery);
+            draghotspot(dragging);
+            ,
+            if(pressed,
+            sub(dx,mouse.stagex,drag_adjustx);
+            sub(dy,mouse.stagey,drag_adjusty);
+            screentosphere(dx,dy,ath,atv);
+            if(onDrag!==null, onDrag() );
+            delayedcall(0, draghotspot(dragging) );
+            , if(onDragEnd!==null, onDragEnd() );
+            );
+            );
+            ]]>
+        </action>
+</krpano>
